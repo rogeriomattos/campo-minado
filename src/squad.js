00b582f794fb5c2589846fc5570561495ebb8e3e
rogerio.mattos7 <rogeriomattos38@outlook.com>
2020-10-04 11:43:47 -0300

    criando classe Squad

---
+++ src/squad.js
@@ -0,0 +1,25 @@
+class Squad {
+    constructor(value = undefined, isActive = false, isFlag = false ) {
+        this.value = value;
+        this.isActive = isActive;
+        this.isFlag = isFlag;
+    }
+
+    setValue(value) {
+        this.value = value;
+    }
+
+    setIsActive(isActive) {
+        this.isActive = isActive;
+    }
+
+    setIsFlag(isFlag) {
+        this.isFlag = isFlag;
+    }
+
+    isBomb() {
+        return this.value == 'B';
+    }
+}
+
+export default Squad;
